6ee25d6f623eb6086cae4c40eaede83b42e560ed
Hitesh_ S <hiteshsingal13@gmail.com>
2025-06-25 11:04:39 +0530

    INTERSHIPP

---
+++ phy1.py
@@ -0,0 +1,20 @@
+# # print("Hii Python","Welcome","!",sep="--->>>", end="====>>>")
+# # print("Hellow", end="__?__")
+# # print("Hellow")
+# # print(10>5)
+# # a=10
+# # b=5
+# # print(f"The sum {a} and {b} number", f"{a/b:.5f}", sep=" = ")
+# # a = int(input("My name is:"))
+# # print(a+ 67)
+# a = 23
+# print(type(a))
+# b= "python"
+# print(b.title())
+#
+# print(type(b))
+str = "This is phython"
+ # print(str.replace(__old=is, _new=Is Great))
+print("String before splitting" + str)
+print(str.splitlines())
+
